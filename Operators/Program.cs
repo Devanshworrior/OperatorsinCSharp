fe81428b5636165a404349db7ec5bb50c4ce547c
Devanshworrior <devanshjangid269@gmail.com>
2024-04-16 20:54:23 +0530

    first commit

---
+++ Operators/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.   Arithmetical Operators
+            //int a = 4;
+            //int b = 2;
+            //Console.WriteLine("Sum of a + b : " + (a + b));
+            //Console.WriteLine("Multi. of a * b : " +(a * b));
+            //Console.WriteLine("Sub of a + b : " + (a * b));
+            //Console.ReadLine();
+
+            //2.  Assignment Operators
+            //int a = 4;
+            //int b = a;
+            //b -= 1;
+            //b += 5;
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+
+            //3.  Logical Operators
+
+            // And
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // Or
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(false || false);
+
+            //Not
+            //Console.WriteLine(!true);
+            //Console.WriteLine(!false);
+            //Console.ReadLine();
+
+            // 4.  Comparison Operators
+            Console.WriteLine(324 > 555);
+            Console.WriteLine(324 <= 555);
+            Console.WriteLine(324 >= 555);
+            Console.WriteLine(324 != 555);
+            Console.ReadLine();
+        }
+    }
+}
